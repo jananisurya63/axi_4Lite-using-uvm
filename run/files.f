09a43f1128ff8f147ee3ddff89c05f19e8c688c5
JANANI SURYA <jananisurya63@gmail.com>
2026-08-10 09:32:23 +0530

    Create files.f

---
+++ run/files.f
@@ -0,0 +1,16 @@
++incdir+/home/dvft1101/final_project/axi4Lite/verification/agent/
++incdir+/home/dvft1101/final_project/axi4Lite/verification/env/
++incdir+/home/dvft1101/final_project/axi4Lite/verification/test/
++incdir+/home/dvft1101/final_project/axi4Lite/verification/tb/
+
+/home/dvft1101/final_project/axi4Lite/verification/tb/axi_interface.sv
+
+
+/home/dvft1101/final_project/axi4Lite/verification/agent/axi_agent_package.svp
+/home/dvft1101/final_project/axi4Lite/verification/env/axi_env_package.svp
+/home/dvft1101/final_project/axi4Lite/verification/test/axi_test_package.svp
+
+
+/home/dvft1101/final_project/axi4Lite/verification/tb/axi_assertion.sv
+/home/dvft1101/final_project/axi4Lite/design/design.sv
+/home/dvft1101/final_project/axi4Lite/verification/tb/axi_top.sv
